09dabcaa7e46251547abd70406a1157071b99d1f
Lauro, Jhon Gil <jhongil.lauro@cit.edu>
2026-05-16 19:51:55 +0800

    Delete src directory

--- src/App.test.js
+++
@@ -1,3 +0,0 @@
-test('Trevora frontend test runner is configured', () => {
-  expect('Trevora').toMatch(/Trevora/);
-});
--- src/components/LoadingScreen.jsx
+++
@@ -1,9 +0,0 @@
-import './LoadingScreen.css';
-
-const LoadingScreen = () => (
-  <div className="loading-screen">
-    <span className="loading-spinner" />
-  </div>
-);
-
-export default LoadingScreen;
